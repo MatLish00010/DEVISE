0124842bcf17549a32d6be8002585f86d1942774
Matvey <lishay.matvey20001506@gmail.com>
2019-10-29 23:10:17 +0300

    fix js file , fix animation
    but you can verify the work only at the merger

--- JS/script.js
+++ JS/script.js
@@ -1,67 +1,38 @@
-var link = document.querySelector('.button__address');
-var popup = document.querySelector('.modal');
-var close = popup.querySelector('.modal__close');
+var popupWriteLink = document.querySelector(".main-contacts__btn"),
+    popupWrite = document.querySelector(".popup-write-us"),
+    userName = popupWrite.querySelector("[name=user-name]"),
+    userEmail = popupWrite.querySelector("[name=user-email]"),
+    popupWriteForm = popupWrite.querySelector("form"),
+    popupMapLink = document.querySelector(".main-contacts__map"),
+    popupMap = document.querySelector(".popup-map"),
+    popupWriteClose = document.querySelector(".popup-write-us__close"),
+    popupMapClose = document.querySelector(".popup-map__close");
 
-var shadow = document.querySelector('.modal__overlay');
+popupWriteLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupWrite.classList.add("popup-write-us--active");
+    userName.focus();
+})
 
-var form = popup.querySelector('.modal__form');
-var login = popup.querySelector('.modal__input--login');
-var mail = popup.querySelector('.modal__input--mail');
-
-var storage = localStorage.getItem('modal__input--login');
-
-// Открытие окна и тени
-link.addEventListener('click', function (event) {
-    event.preventDefault();
-    popup.classList.add('modal__show');
-    shadow.classList.add('modal__overlay-shadow');
-      if (storage) {
-        login.value = storage;
-        mail.focus();
-    } else {
-        login.focus();
+popupWriteForm.addEventListener("submit", function (evt) {
+    if (!userName.value || !userEmail.value) {
+        evt.preventDefault();
+        popupWrite.classList.add("popup-write-us--error");
     }
-});
+})
 
-
-// закрытие окна
-close.addEventListener('click', function (event) {
-    event.preventDefault();
-    popup.classList.remove('modal__show');
-    popup.classList.remove('modal__error');
-    shadow.classList.remove('modal__overlay-shadow');
-});
-
-// отправка формы
-form.addEventListener('submit', function (event) {
-        if (!login.value || !password.value) {
-        popup.classList.remove('modal__error');
-        //popup.offsetWidth = popup.offsetWidth;
-        console.log('Нужно ввести имя и электронную почту');
-        popup.classList.add('modal__error');
-        event.preventDefault();
-    } else {
-        localStorage.setItem('login', login.value);
-        localStorage.setItem('mail', password.value);
-    }
-});
-
-// закрытие на esc
-window.addEventListener('keydown', function (event) {
-    if (event.keyCode === 27) {
-        if (popup.classList.contains('modal__show')) {
-            popup.classList.remove('modal__show');
-            popup.classList.remove('modal__error');
-            shadow.classList.remove('modal__overlay-shadow');
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popupWrite.classList.contains("popup-write-us--active")) {
+            popupWrite.classList.remove("popup-write-us--active");
+            popupWrite.classList.remove("popup-write-us--error");
         }
     }
-});
+})
 
-
-// закрытие при нажатии на тень
-shadow.addEventListener("click", function(event) {
-    event.preventDefault();
-    popup.classList.remove('modal__show');
-    popup.classList.remove('modal__error');
-    shadow.classList.remove('modal__overlay-shadow');
-});
+popupWriteClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupWrite.classList.remove("popup-write-us--active");
+    popupWrite.classList.remove("popup-write-us--error");
+})
