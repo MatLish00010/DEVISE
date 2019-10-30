629a9671125449e3a41a1998aefd8fa06b9eb633
Matvey <lishay.matvey20001506@gmail.com>
2019-10-31 01:05:40 +0300

    window map complite

--- JS/script for maps.js
+++ JS/script for maps.js
@@ -4,11 +4,6 @@ var close = popup.querySelector('.window__close');
 
 var shadow = document.querySelector('.window__overlay');
 
-var form = popup.querySelector('.modal__form');
-var login = popup.querySelector('.modal__input--login');
-var mail = popup.querySelector('.modal__input--mail');
-
-var storage = localStorage.getItem('modal__input--login');
 
 // Открытие окна и тени
 link.addEventListener('click', function (event) {
@@ -32,27 +27,14 @@ close.addEventListener('click', function (event) {
     shadow.classList.remove('window__overlay-shadow');
 });
 
-// отправка формы
-form.addEventListener('submit', function (event) {
-        if (!login.value || !password.value) {
-        popup.classList.remove('modal__error');
-        //popup.offsetWidth = popup.offsetWidth;
-        console.log('Нужно ввести имя и электронную почту');
-        popup.classList.add('modal__error');
-        event.preventDefault();
-    } else {
-        localStorage.setItem('login', login.value);
-        localStorage.setItem('mail', password.value);
-    }
-});
 
 // закрытие на esc
 window.addEventListener('keydown', function (event) {
     if (event.keyCode === 27) {
-        if (popup.classList.contains('modal__show')) {
-            popup.classList.remove('modal__show');
-            popup.classList.remove('modal__error');
-            shadow.classList.remove('modal__overlay-shadow');
+        if (popup.classList.contains('window-show')) {
+            popup.classList.remove('window-show');
+            popup.classList.remove('window__error');
+            shadow.classList.remove('window__overlay-shadow');
         }
     }
 });
@@ -61,7 +43,7 @@ window.addEventListener('keydown', function (event) {
 // закрытие при нажатии на тень
 shadow.addEventListener("click", function(event) {
     event.preventDefault();
-    popup.classList.remove('modal__show');
-    popup.classList.remove('modal__error');
-    shadow.classList.remove('modal__overlay-shadow');
+    popup.classList.remove('window-show');
+    popup.classList.remove('window__error');
+    shadow.classList.remove('window__overlay-shadow');
 });
